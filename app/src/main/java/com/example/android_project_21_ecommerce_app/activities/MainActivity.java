0d30084a8cc5b130e2c69d917b9de70e9ad22af5
footmanphp <footmanphp@gmail.com>
2021-08-22 22:25:00 +0430

    new

--- app/src/main/java/com/example/android_project_21_ecommerce_app/activities/MainActivity.java
+++ app/src/main/java/com/example/android_project_21_ecommerce_app/activities/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.android_project_21_ecommerce_app.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.android_project_21_ecommerce_app.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_login;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    public void initViews() {
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(this);
+        btn_register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                break;
+
+            case R.id.btn_register:
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                break;
+        }
     }
 }
